8af5844eccdd90aeffe58cd51ecfcdd3a9bc01e8
hohohoo <sese7406@sogang.ac.kr>
2015-08-03 02:00:19 +0900

    Create euler6.java

---
+++ euler6.java
@@ -0,0 +1,9 @@
+public class euler6 {
+
+	public static void main(String[] args){
+		
+		int t = 100;
+		int result = (t*(t+1)/2)*(t*(t+1)/2)-t*(t+1)*(2*t+1)/6;  
+		System.out.println(result);
+	}
+}
